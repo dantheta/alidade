bc7c506b6494f48360f2353f79aac684cdf046f7
Daniel Ramsay <daniel@dretzq.org.uk>
2019-09-25 22:47:07 +0100

    fix step heading title #7

--- app/view/project/sidebar.php
+++ app/view/project/sidebar.php
@@ -12,7 +12,7 @@ $piece = ($inTour ? 'tour' : 'slide');
              } ?>
     <div class="step step<?php echo $step; ?> <?php echo ($currs[0] == 1 ? '' : 'hidden-xs'); ?>">
   		<header>
-  			<h3><a class="slide <?php echo checkSlidePosition($step_number, $slide_number, 1, 0); ?>" href="/project/<?php echo $piece; ?>/1.0<?php echo (!is_null($hash) ? '/?p=' . $hash : ''); ?>">STEP 1</a></h3>
+          <h3><a class="slide <?php echo checkSlidePosition($step_number, $slide_number, 1, 0); ?>" href="/project/<?php echo $piece; ?>/1.0<?php echo (!is_null($hash) ? '/?p=' . $hash : ''); ?>">STEP <?php echo $step ?></a></h3>
   		</header>
   		<ul>
   			<?php for($i = 1; $i <= count($entries); $i++){ ?>
