00e8ee36d1d21c940cea1e39473ce235c2d555d9
Daniel Ramsay <daniel@dretzq.org.uk>
2019-12-20 01:51:41 +0000

    styling updates

--- app/view/header.php
+++ app/view/header.php
@@ -89,7 +89,7 @@
               </a>
             </div>
         </div>
-        <div class="row">
+        <div id="app-header-row2" class="row">
             <div class="col-md-8 col-sm-6 col-xs-4">
             </div>
             <div class="col-md-4 col-sm-6 col-xs-8">
